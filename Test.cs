52913ac8eff785dd056f0e34bd557e73d5924035
KRISHNAPRASAD AWALA <krishnaprasadawala@gmail.com>
2023-01-28 13:08:39 +0530

    Some more code

--- Test.cs
+++ Test.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    
+    Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
         // Debug.Log("Hello World!");
-        Destroy(gameObject);
+        // To destroy an object in 3 seconds
+        // Destroy(gameObject,3f);
+
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
-        
+
+        //This code works when space bar is pressed.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Destroy(gameObject);
+
+            // Adds Force in upward direction when space is clicked.
+            rb.AddForce(Vector3.up * 500);
+
+        }
+
+        // Adds velocity to the object. The object keeps on moving forward. 
+        rb.velocity = Vector3.forward * 20f;
+
+
+    }
+
+
+    //This function destroys object when mouse is clicked.
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
     }
 }
